a3172a09f332a4239580f18643f96739c94430ca
René Anderes <randeres@yahoo.de>
2013-08-24 17:29:56 +0200

    Zweiter Push

---
+++ git/src/test/java/org/anderes/payground/git/MyGitClassTest.java
@@ -0,0 +1,14 @@
+package org.anderes.payground.git;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyGitClassTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
